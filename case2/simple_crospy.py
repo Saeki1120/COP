b5fd7daf9252aa07d152f6d12b6f4cfa6a8220a3
Saeki1120 <y_saeki@f.ait.kyushu-u.ac.jp>
2019-02-10 17:41:40 +0900

    メソッドを追加

--- case2/simple_crospy.py
+++ case2/simple_crospy.py
@@ -5,19 +5,31 @@ import sys
 from crospy import *
 
 class Test1(CPy):
-    def __init__(self):
+    def __init__(self, dim1_1='dim1_1', dim2_1='dim2_1'):
         super(Test1, self).__init__()
-        self.dim1 = "dim1"
+        self.dim1_1 = "dim1_1"
+        self.dim2_1 = "dim2_1"
 
     @cpybase
     def context_method1_1(self):
-        self.dim1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
-        print self.dim1
+        self.dim1_1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim1_1
+
+    @cpybase
+    def context_method2_1(self):
+        self.dim2_1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim2_1
 
 @cpylayer(Test1, 'layer1_1', 'context_method1_1')
 def context_method1_1(self):
-    self.dim1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
-    print self.dim1
+    self.dim1_1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+    print self.dim1_1
+    self.proceed()
+
+@cpylayer(Test1, 'layer1_1', 'context_method2_1')
+def context_method2_1(self):
+    self.dim2_1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+    print self.dim2_1
     self.proceed()
 
 
@@ -27,6 +39,8 @@ if __name__ == '__main__':
 
     t1 = Test1()
     t1.context_method1_1()
+    t1.context_method2_1()
     print ("------------------------")
     t1.activate('layer1_1')
     t1.context_method1_1()
+    t1.context_method2_1()
--- case2/simple_primitive.py
+++ case2/simple_primitive.py
@@ -5,9 +5,10 @@ import sys
 from my_context import *
 
 class Test1(Context):
-    def __init__(self, arg=''):
+    def __init__(self, dim1_1='dim1_1', dim2_1='dim2_1'):
         super(Test1, self).__init__()
-        self.dim1 = "dim1"
+        self.dim1_1 = dim1_1
+        self.dim2_1 = dim2_1
 
     def context_method1_1(self):
         for l in reversed(self.layers):
@@ -18,13 +19,30 @@ class Test1(Context):
             else:
                 pass
 
+    def context_method2_1(self):
+        for l in reversed(self.layers):
+            if 'base' == l:
+                self.context_method2_1_base()
+            elif 'layer1_1' == l:
+                self.context_method2_1_layer1_1()
+            else:
+                pass
+
     def context_method1_1_base(self):
-        self.dim1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
-        print self.dim1
+        self.dim1_1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim1_1
 
     def context_method1_1_layer1_1(self):
-        self.dim1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
-        print self.dim1
+        self.dim1_1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim1_1
+
+    def context_method2_1_base(self):
+        self.dim2_1 = "base : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim2_1
+
+    def context_method2_1_layer1_1(self):
+        self.dim2_1 = "layer1_1 : " + self.__class__.__name__ + " : " + sys._getframe().f_code.co_name
+        print self.dim2_1
 
 
 # Main
@@ -33,6 +51,8 @@ if __name__ == '__main__':
 
     t1 = Test1()
     t1.context_method1_1()
+    t1.context_method2_1()
     print ("------------------------")
     t1.activate('layer1_1')
     t1.context_method1_1()
+    t1.context_method2_1()
